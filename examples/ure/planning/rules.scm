a8451a5f367ff96bdc85e2ed84e1edc7ebb17340
Raschild6 <michele.thiella@studenti.unipd.it>
2021-03-22 21:04:25 +0100

    init local repo

--- examples/ure/planning/rules.scm
+++ examples/ure/planning/rules.scm
@@ -1,8 +1,8 @@
-(use-modules (opencog) (opencog query) (opencog exec))
+(use-modules (opencog) (opencog exec))
 
 (define (action . args)
- (let ((tmp (car args)))
-   tmp
+	(let ((tmp (car args)))
+   	tmp
    )
 )
 
@@ -13,254 +13,264 @@
 
 (define pickup 
 	(BindLink
-          (VariableList
-	    (TypedVariableLink
-               (VariableNode "?ob") (TypeNode "ConceptNode"))
+   	(VariableList
+	   	(TypedVariableLink (VariableNode "?ob") (TypeNode "ConceptNode"))
 	   ) ; parameters
-           (AndLink
-              (EvaluationLink
-		(PredicateNode "clear")
-           	(VariableNode "?ob"))
-              (EvaluationLink
-           	(PredicateNode "on-table")
-           	(VariableNode "?ob"))      
-	      (InheritanceLink
-		(VariableNode "?ob")
-		(ConceptNode "object")))
-            (ExecutionOutputLink
-	      (GroundedSchemaNode "scm: pickup-action")
-	      (ListLink
-		(AndLink ; effect
-                    (EvaluationLink
-                      (PredicateNode "holding")
-                      (VariableNode "?ob"))
-                    (EvaluationLink
-                      (PredicateNode "not-clear")
-                      (VariableNode "?ob"))
-                    (EvaluationLink
-                      (PredicateNode "not-on-table")
-                      (VariableNode "?ob"))
-		   ; (DeleteLink
-                   ;   (EvaluationLink
-                   ;     (PredicateNode "not-holding")
-                   ;     (VariableNode "?ob")))
-                   ; (DeleteLink
-		   ;   (EvaluationLink
-                   ;     (PredicateNode "clear")
-                   ;     (VariableNode "?ob")))
-		   ; (DeleteLink
-		   ;   (EvaluationLink
-                   ;     (PredicateNode "on-table")
-                   ;     (VariableNode "?ob")))
-		)
-		(AndLink ; precondition
-		  (EvaluationLink
-                     (PredicateNode "clear")
-                     (VariableNode "?ob"))
-                  (EvaluationLink
-                      (PredicateNode "on-table")
-			(VariableNode "?ob")))
-	    ))))
-
-(define putdown
-  (BindLink
-    (VariableList
-      (TypedVariableLink (VariableNode "?ob") (TypeNode "ConceptNode"))
-    ) ; parameters
-    (AndLink
-      (PresentLink
-	(EvaluationLink
-	  (PredicateNode "holding")
-	  (VariableNode "?ob")))
-	(InheritanceLink
-		(VariableNode "?ob")
-		(ConceptNode "object"))
-      )
-    (ExecutionOutputLink        
-	(GroundedSchemaNode "scm: putdown-anction")
-	(ListLink
-	  (AndLink ; effect
-	      (EvaluationLink
-		(PredicateNode "clear")
-		(VariableNode "?ob"))
-	      (EvaluationLink
-		(PredicateNode "on-table")
-		(VariableNode "?ob"))
-	      (EvaluationLink
-		(PredicateNode "not-holding")
-		(VariableNode "?ob"))
-;	      (DeleteLink
-;	        (EvaluationLink
-;		  (PredicateNode "holding")
-;		  (VariableNode "?ob")))
-          )
-	  (AndLink    ; precondition
-	    (EvaluationLink
-          	(PredicateNode "holding")
-          	(VariableNode "?ob")))))
-    ))
-
-(define stack
-  (BindLink
-    (VariableList
-      (TypedVariableLink
-            (VariableNode "?ob") (TypeNode "ConceptNode"))
-      (TypedVariableLink 
-	(VariableNode "?underob") (TypeNode "ConceptNode"))
-    ) ; parameters
-    (AndLink
-      (NotLink
-	(EqualLink (VariableNode "?ob") (VariableNode "?underob")))
-      (PresentLink
-         (EvaluationLink
-            (PredicateNode "clear")
-            (VariableNode  "?underob")))
-      (InheritanceLink
-		(VariableNode "?ob")
-		(ConceptNode "object"))
-      (InheritanceLink
-		(VariableNode "?underob")
-		(ConceptNode "object"))
-      (PresentLink
-         (EvaluationLink
-            (PredicateNode "holding")
-            (VariableNode "?ob"))))
-    (ExecutionOutputLink         
-        (GroundedSchemaNode "scm: stack-action")
-        (ListLink
-          (AndLink ; effect
-	     (EvaluationLink
-                (PredicateNode "clear")
-		(VariableNode  "?ob"))
-	     (EvaluationLink
-	       (PredicateNode "on")
-	       (ListLink
-		 (VariableNode "?ob")
-		 (VariableNode "?underob")))
-	     (EvaluationLink
-	       (PredicateNode "not-clear")
-	       (VariableNode "?underob"))
-	    ; (DeleteLink
-     	    ;   (EvaluationLink
-	    ;      (PredicateNode "clear")
-	    ;      (VariableNode "?underob")))
-	    ; (DeleteLink
-	    ;   (EvaluationLink
-	    ;     (PredicateNode "holding")
-	    ;     (VariableNode "?ob")))
-	     (EvaluationLink
-	       (PredicateNode "not-holding")
-	       (VariableNode "?ob")))
-	  (AndLink ; preconditon
-	    (EvaluationLink
-                (PredicateNode "clear")
-                (VariableNode  "?underob"))
-	    (EvaluationLink
-                (PredicateNode "holding")
-                (VariableNode "?ob")))))
-    ))
-
-(define unstack
-    (BindLink
-    (VariableList
-      (TypedVariableLink 
-	(VariableNode "?ob") (TypeNode "ConceptNode"))
-      (TypedVariableLink
-	(VariableNode "?underob") (TypeNode "ConceptNode"))
-    ) ; parameters
-    (AndLink
-      (NotLink
-	(EqualLink (VariableNode "?ob") (VariableNode "?underob")))
-      (PresentLink
-	(EvaluationLink
-	  (PredicateNode "on")
-	  (ListLink
-	     (VariableNode "?ob")
-	     (VariableNode "?underob"))))
-      (InheritanceLink
-		(VariableNode "?underob")
-		(ConceptNode "object"))
-      (InheritanceLink
-		(VariableNode "?ob")
-		(ConceptNode "object"))
-      (PresentLink
-        (EvaluationLink
-           (PredicateNode "clear")
-           (VariableNode  "?ob"))))
-    (ExecutionOutputLink
-      (GroundedSchemaNode "scm: unstack-action")
-      (ListLink
-	(AndLink ; effect
-           (EvaluationLink
-                (PredicateNode "holding")
-                (VariableNode "?ob"))
-           (EvaluationLink
-                (PredicateNode "clear")
-		(VariableNode  "?underob"))
-	   (EvaluationLink
-	     (PredicateNode "not-on")
-	     (ListLink
-	        (VariableNode "?ob")
-	        (VariableNode "?underob")))
-	   (EvaluationLink
-	     (PredicateNode "not-clear")
-	     (VariableNode "?ob")))
-	(AndLink ; preconditions
-            (EvaluationLink
-              (PredicateNode "on")
-              (ListLink
-                 (VariableNode "?ob")
-                 (VariableNode "?underob")))
-            (EvaluationLink
-               (PredicateNode "clear")
-               (VariableNode  "?ob")))))
-    ))
-
-(define (dummy-elim . args)
-  (car args)
+	   (AndLink
+	   	(EvaluationLink
+				(PredicateNode "clear")
+				(VariableNode "?ob"))
+			(EvaluationLink
+				(PredicateNode "on-table")
+				(VariableNode "?ob"))
+			(InheritanceLink
+				(VariableNode "?ob")
+				(ConceptNode "object")))
+		(ExecutionOutputLink
+			(GroundedSchemaNode "scm: pickup-action")
+			(ListLink
+				(AndLink ; effect
+					(EvaluationLink
+						(PredicateNode "holding")
+						(VariableNode "?ob"))
+					(EvaluationLink
+						(PredicateNode "not-clear")
+						(VariableNode "?ob"))
+					(EvaluationLink
+						(PredicateNode "not-on-table")
+						(VariableNode "?ob"))
+					;(DeleteLink
+						;(EvaluationLink
+							;(PredicateNode "not-holding")
+							;(VariableNode "?ob")))
+					;(DeleteLink
+						;(EvaluationLink
+							;(PredicateNode "clear")
+							;(VariableNode "?ob")))
+					;(DeleteLink
+						;(EvaluationLink
+							;(PredicateNode "on-table")
+							;(VariableNode "?ob")))
+				)
+				(AndLink ; precondition
+					(EvaluationLink
+						(PredicateNode "clear")
+						(VariableNode "?ob"))
+					(EvaluationLink
+						(PredicateNode "on-table")
+						(VariableNode "?ob")))
+			)
+	   )
+	)
 )
 
+(define putdown
+	(BindLink
+   	(VariableList
+      	(TypedVariableLink (VariableNode "?ob") (TypeNode "ConceptNode"))
+    	) ; parameters
+		(AndLink
+			(PresentLink
+				(EvaluationLink
+					(PredicateNode "holding")
+					(VariableNode "?ob")))
+				(InheritanceLink
+					(VariableNode "?ob")
+					(ConceptNode "object"))
+		)
+		(ExecutionOutputLink
+			(GroundedSchemaNode "scm: putdown-action")
+			(ListLink
+				(AndLink ; effect
+					(EvaluationLink
+						(PredicateNode "clear")
+						(VariableNode "?ob"))
+					(EvaluationLink
+						(PredicateNode "on-table")
+						(VariableNode "?ob"))
+					(EvaluationLink
+						(PredicateNode "not-holding")
+						(VariableNode "?ob"))
+					;(DeleteLink
+					;	(EvaluationLink
+					;		(PredicateNode "holding")
+					;		(VariableNode "?ob")))
+				)
+				(AndLink    ; precondition
+					(EvaluationLink
+						(PredicateNode "holding")
+						(VariableNode "?ob")))
+			)
+		)
+	)
+)
+
+(define stack
+	(BindLink
+   	(VariableList
+			(TypedVariableLink (VariableNode "?ob") (TypeNode "ConceptNode"))
+			(TypedVariableLink (VariableNode "?underob") (TypeNode "ConceptNode"))
+		) ; parameters
+		(AndLink
+			(NotLink
+				(EqualLink (VariableNode "?ob") (VariableNode "?underob")))
+			(PresentLink
+				(EvaluationLink
+					(PredicateNode "clear")
+					(VariableNode  "?underob")))
+			(InheritanceLink
+				(VariableNode "?ob")
+				(ConceptNode "object"))
+			(InheritanceLink
+				(VariableNode "?underob")
+				(ConceptNode "object"))
+			(PresentLink
+				(EvaluationLink
+					(PredicateNode "holding")
+					(VariableNode "?ob"))))
+		(ExecutionOutputLink
+			(GroundedSchemaNode "scm: stack-action")
+			(ListLink
+				(AndLink ; effect
+					(EvaluationLink
+						(PredicateNode "clear")
+						(VariableNode  "?ob"))
+					(EvaluationLink
+						(PredicateNode "on")
+						(ListLink
+							(VariableNode "?ob")
+							(VariableNode "?underob")))
+					(EvaluationLink
+						(PredicateNode "not-clear")
+						(VariableNode "?underob"))
+					;(DeleteLink
+					;	(EvaluationLink
+					;		(PredicateNode "clear")
+					;		(VariableNode "?underob")))
+					;(DeleteLink
+					;	(EvaluationLink
+					;		(PredicateNode "holding")
+					;		(VariableNode "?ob")))
+					(EvaluationLink
+						(PredicateNode "not-holding")
+						(VariableNode "?ob")))
+				(AndLink ; preconditon
+					(EvaluationLink
+						(PredicateNode "clear")
+						(VariableNode  "?underob"))
+					(EvaluationLink
+						(PredicateNode "holding")
+						(VariableNode "?ob")))
+			)
+		)
+	)
+)
+
+(define unstack
+	(BindLink
+		(VariableList
+			(TypedVariableLink (VariableNode "?ob") (TypeNode "ConceptNode"))
+			(TypedVariableLink (VariableNode "?underob") (TypeNode "ConceptNode"))
+		) ; parameters
+		(AndLink
+			(NotLink
+				(EqualLink (VariableNode "?ob") (VariableNode "?underob")))
+			(PresentLink
+				(EvaluationLink
+					(PredicateNode "on")
+					(ListLink
+						(VariableNode "?ob")
+						(VariableNode "?underob"))))
+			(InheritanceLink
+				(VariableNode "?underob")
+				(ConceptNode "object"))
+			(InheritanceLink
+				(VariableNode "?ob")
+				(ConceptNode "object"))
+			(PresentLink
+				(EvaluationLink
+					(PredicateNode "clear")
+					(VariableNode  "?ob"))))
+		(ExecutionOutputLink
+			(GroundedSchemaNode "scm: unstack-action")
+			(ListLink
+				(AndLink ; effect
+					(EvaluationLink
+						(PredicateNode "holding")
+						(VariableNode "?ob"))
+					(EvaluationLink
+						(PredicateNode "clear")
+						(VariableNode  "?underob"))
+					(EvaluationLink
+						(PredicateNode "not-on")
+						(ListLink
+							(VariableNode "?ob")
+							(VariableNode "?underob")))
+					(EvaluationLink
+						(PredicateNode "not-clear")
+						(VariableNode "?ob")))
+				(AndLink ; preconditions
+					(EvaluationLink
+						(PredicateNode "on")
+						(ListLink
+							(VariableNode "?ob")
+							(VariableNode "?underob")))
+					(EvaluationLink
+						(PredicateNode "clear")
+						(VariableNode  "?ob")))
+			)
+		)
+	)
+)
+
+(define (dummy-elim . args) (car args))
 
 (define dummy-intro dummy-elim)
 
-
 ;; Generate a list of variables (Variable prefix + "-" + to_string(n))
 (define (gen-variables prefix n)
-  (if (= n 0)
-      ;; Base case
+	(if (= n 0)
+  		;; Base case
       '()
       ;; Recursive case
       (append (gen-variables prefix (- n 1))
-              (list (gen-variable prefix (- n 1))))))
+              (list (gen-variable prefix (- n 1))))
+   )
+)
 
 (define (lastElem list) (car (reverse list)))
+
+
 ;; Generate a fuzzy conjunction elemination rule for an n-ary
 ;; conjunction
 (define (gen-conjunction-elemination-rule nary)
-  (let* ((variables (gen-variables "$X" nary))
-         (EvaluationT (Type "EvaluationLink"))
-         (DeleteT (Type "DeleteLink"))
-         (InheritanceT (Type "InheritanceLink"))
-         (type (TypeChoice EvaluationT InheritanceT DeleteT))
-         (gen-typed-variable (lambda (x) (TypedVariable x type)))
-         (vardecl (VariableList (map gen-typed-variable variables)))
-         (pattern (AndLink variables))
-         (rewrite (ExecutionOutput
-                    (GroundedSchema "scm: dummy-elim")
-                    ;; We wrap the variables in Set because the order
-                    ;; doesn't matter and that way alpha-conversion
-                    ;; works better.
-                    (List (car variables) (And variables)))))
-    (Bind
-      vardecl
-      pattern
-      rewrite)))
+	(let* ((variables (gen-variables "$X" nary))
+			(EvaluationT (Type "EvaluationLink"))
+			(DeleteT (Type "DeleteLink"))
+			(InheritanceT (Type "InheritanceLink"))
+			(type (TypeChoice EvaluationT InheritanceT DeleteT))
+			(gen-typed-variable (lambda (x) (TypedVariable x type)))
+			(vardecl (VariableList (map gen-typed-variable variables)))
+			(pattern (AndLink variables))
+			(rewrite (ExecutionOutput
+							(GroundedSchema "scm: dummy-elim")
+							;; We wrap the variables in Set because the order
+							;; doesn't matter and that way alpha-conversion
+							;; works better.
+							(List (car variables) (And variables)))
+			))
+			(Bind
+				vardecl
+				pattern
+				rewrite)
+	)
+)
 
 
 ;; Generate a fuzzy conjunction introduction rule for an n-ary
 ;; conjunction
 (define (gen-conjunction-introduction-rule nary)
-  (let* ((variables (gen-variables "$X" nary))
+	(let* ((variables (gen-variables "$X" nary))
          (EvaluationT (Type "EvaluationLink"))
          (DeleteT (Type "DeleteLink"))
          (InheritanceT (Type "InheritanceLink"))
@@ -270,15 +280,17 @@
          (vardecl (VariableList (map gen-typed-variable variables)))
          (pattern (Present variables))
          (rewrite (ExecutionOutput
-                    (GroundedSchema "scm: dummy-intro")
-                    ;; We wrap the variables in Set because the order
-                    ;; doesn't matter and that way alpha-conversion
-                    ;; works better.
-                    (List (And variables) (Set variables)))))
-    (Bind
-      vardecl
-      pattern
-      rewrite)))
+                  	(GroundedSchema "scm: dummy-intro")
+                    	;; We wrap the variables in Set because the order
+                    	;; doesn't matter and that way alpha-conversion
+                    	;; works better.
+                    	(List (And variables) (Set variables)))))
+   		(Bind
+   			vardecl
+      		pattern
+      		rewrite)
+   )
+)
 
 
 (define (replace_in_bind bindlink arguments)
@@ -286,12 +298,13 @@
 
 
 (define (add-to-rule-base bindlink name rbs)
-    (DefineLink
-      (DefinedSchemaNode name)
+	(DefineLink
+   	(DefinedSchemaNode name)
       bindlink)
-    (MemberLink
-        (DefinedSchemaNode name)
-        rbs))
+   (MemberLink
+   	(DefinedSchemaNode name)
+   	rbs)
+)
 
 (define rbs (ConceptNode "block-world"))
 (add-to-rule-base pickup "pickup" rbs)
--- examples/ure/planning/test.scm
+++ examples/ure/planning/test.scm
@@ -2,18 +2,24 @@
 (import (opencog logger))
 
 (load "rules.scm")
+; If run this file step-by-step use this line instead
+;(load "allExamples/ure/planning/rules.scm")
 
 
 (cog-logger-set-level! (cog-ure-logger) "fine")
+(cog-logger-set-filename! "examples_log/log_blocksworld.log")
 (define (compute)
 
    (define init (SetLink
-       ; define objects
-       (InheritanceLink (stv 1 1)
-         (ConceptNode "a")
-         (ConceptNode "object"))
-       (InheritanceLink (stv 1 1)
+   	; define objects
+      (InheritanceLink (stv 1 1)
+        (ConceptNode "a")
+        (ConceptNode "object"))
+      (InheritanceLink (stv 1 1)
          (ConceptNode "b")
+         (ConceptNode "object"))
+      (InheritanceLink (stv 1 1)
+         (ConceptNode "c")
          (ConceptNode "object"))
       (EvaluationLink (stv 1 1)
          (PredicateNode "on-table")
@@ -21,28 +27,41 @@
       (EvaluationLink (stv 1 1)
          (PredicateNode "on-table")
          (ConceptNode "b"))
-       (EvaluationLink (stv 1 1)
+      (EvaluationLink (stv 1 1)
+         (PredicateNode "on-table")
+         (ConceptNode "c"))
+      (EvaluationLink (stv 1 1)
+      	(PredicateNode "clear")
+      	(ConceptNode "a"))
+      (EvaluationLink (stv 1 1)
          (PredicateNode "clear")
-         (ConceptNode "a"))
-       (EvaluationLink (stv 1 1)
+         (ConceptNode "b"))
+		(EvaluationLink (stv 1 1)
          (PredicateNode "clear")
-         (ConceptNode "b")))
+         (ConceptNode "c"))
+      )
    )
-   (define goal (AndLink
-		  (EvaluationLink 
-		    (PredicateNode "on")
-		    (ListLink
-		      (VariableNode "x")
-		      (ConceptNode "b")))))
-
-   (display goal)
-   (cog-bc rbs goal)
-   ; (cog-bc rbs goal1)
-   ;(cog-fc rbs init)
+	(define goal (AndLink
+		(EvaluationLink
+			(PredicateNode "on")
+			(ListLink
+				(VariableNode "x")
+				(ConceptNode "b")))))
+	;(display goal)
+	(cog-fc rbs init)
+	(cog-bc rbs goal)
 )
 
 (define rbs (ConceptNode "block-world"))
-(ure-set-maximum-iterations rbs 200)
+(ure-set-maximum-iterations rbs 120)
+;(define rbs2 (ConceptNode "block-world2"))
+;(ure-set-maximum-iterations rbs2 1)
 (define result (compute))
-(display result)(newline)
+;(display result)(newline)
+
+(let ((output-port (open-file "examples_log/result_blocksworld.txt" "a")))
+  (display result output-port)
+  (newline output-port)
+  (close output-port))
+
 
